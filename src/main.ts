ba7c84fa102a22207600a5b7a27d9df5f93873bd
ArzubekDev <arzubekmain@gmail.com>
2026-02-20 18:00:05 +0600

    fix: maints

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,16 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // app.enableCors({
+  //   origin: ['http://localhost:3000', 'https://nexus-alpha-navy.vercel.app'],
+  //   credentials: true,
+  // });
+
   app.enableCors({
-    origin: ['http://localhost:3000', 'https://nexus-alpha-navy.vercel.app'],
-    credentials: true,
-  });
+  origin: true, // Бардык жерден келген сурамдарга уруксат берет
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+  credentials: true,
+});
 
   const port = process.env.PORT || 3000;
 
